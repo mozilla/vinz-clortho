29e910d01283774c6dd6c6fbbbdf77c6a30696cb
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-04-20 16:03:33 -0600

    remove dependency on node-crypto

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const config = require('./lib/configuration'),
-      crypto = require('./lib/crypto.js');
+      crypto = require('./lib/crypto'),
         util = require('util'),
 emailRewrite = require('./lib/email_rewrite.js');
 
