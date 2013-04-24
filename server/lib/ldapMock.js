bb037137dd72d2b3787da4a698844ff2f2ed20b7
Benson Wong <mostlygeek@gmail.com>
2013-04-24 13:43:10 -0700

    add ldapMock server

---
+++ server/lib/ldapMock.js
@@ -0,0 +1,12 @@
+/*
+ * Mock LDAP server, useful for testing and development
+ *
+ */
+
+const ldap = require("ldapjs");
+
+ldapServer = ldap.createServer()
+
+// obviously we need to add a bit more to this :D 
+
+module.exports = exports = ldapServer;
