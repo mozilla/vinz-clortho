1d9d0de462a4a553c45281b7145466443892ca1a
Benson Wong <mostlygeek@gmail.com>
2013-04-24 15:56:44 -0700

    add test server implementation, #27

--- server/lib/ldapMock.js
+++ server/lib/ldapMock.js
@@ -5,8 +5,62 @@
 
 const ldap = require("ldapjs");
 
+// mmmmm... all passwords are "test"
+var directory = [
+    // the vinz clortho binds as this user
+    {dn: "cn=vinz, dc=mozilla, o=com", attributes: { cn: "vinz" }}, 
+
+    {dn: "mail=user@mozilla.com, dc=mozilla, o=com", 
+        attributes: { mail: "user@mozilla.com" }}, 
+
+    {dn: "mail=user@mozilla.org, dc=mozilla, o=com", 
+        attributes: { mail: "user@mozilla.org" }},
+
+    {dn: "mail=user@mozilla.org.localdomain, dc=mozilla, o=com", 
+        attributes: { mail: "user@mozilla.org.localdomain" }},
+
+    {dn: "mail=user@mozilla.org.localdomain, dc=mozilla, o=com", 
+        attributes: { mail: "user@mozilla.org.localdomain" }},
+];
+
 ldapServer = ldap.createServer()
 
-// obviously we need to add a bit more to this :D 
 
-module.exports = exports = ldapServer;
+ldapServer.bind('dc=mozilla, o=com', function(req, res, next) {
+    var bindDN = req.dn.toString();
+    var credentials = req.credentials;
+    for(var i=0; i < directory.length; i++) {
+        if(directory[i].dn === bindDN && credentials == "test") {
+            res.end();
+            return next();
+        }
+    }
+
+    return next(new ldap.InvalidCredentialsError());
+});
+
+// some middleware to make sure the user has successful bound
+function authorize(req, res, next) {
+    for(var i=0; i < directory.length; i++) {
+        if (req.connection.ldap.bindDN.equals(directory[i].dn)) {
+            return next();
+        }
+    }
+    return next(new ldap.InsufficientAccessRightsError());
+}
+
+ldapServer.search('dc=mozilla, o=com', [authorize], function(req, res, next) {
+    directory.forEach(function(user) {
+        if (req.filter.matches(user.attributes)) {
+            res.send(user);
+        }
+    });
+
+    res.end();
+    return next();
+});
+
+module.exports = exports = {
+    directory: directory,
+    server: ldapServer
+};
---
+++ test-server/server.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/**
+ * This just starts our mock ldap server on the default port 
+ */
+
+ldapMock = require("../server/lib/ldapMock")
+ldapMock.server.listen(1389, function() {
+    console.log("Directory Entries: ", JSON.stringify(ldapMock.directory, null, "    "));
+    console.log("Listening on port 1389");
+});
+
+/* Example queries you can run! 
+
+   Get all emails 
+   ---------------------
+   > ldapsearch -H ldap://localhost:1389 -x \
+        -D mail=user@mozilla.com,dc=mozilla,o=com -w test \
+        -LLL -b "dc=mozilla, o=com" mail=*
+
+   Search by email
+   ---------------
+   > ldapsearch -H ldap://localhost:1389 -x \
+        -D mail=user@mozilla.com,dc=mozilla,o=com -w test \
+        -LLL -b "dc=mozilla, o=com" mail=user@mozilla.org 
+
+   Bind wrong w/ invalid user
+   --------------------------
+   > ldapsearch -H ldap://localhost:1389 -x \
+        -D cn=not_here,dc=mozilla,o=com -w test \
+        -LLL -b "dc=mozilla, o=com" mail=user@mozilla.org 
+
+   Bind wrong w/ bad password
+   --------------------------
+   > ldapsearch -H ldap://localhost:1389 -x \
+        -D cn=vinz,dc=mozilla,o=com -w OOPS \
+        -LLL -b "dc=mozilla, o=com" mail=user@mozilla.org 
+
+*/
